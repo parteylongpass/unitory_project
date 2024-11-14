e7a8826fb177563b0a1ae074bbc56b3b91103f38
Yongchan <124363127+parteylongpass@users.noreply.github.com>
2024-11-14 20:12:09 +0900

    홈 화면 작업 -페이지네이션 구현 완료

--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:unitory_project/Item/view/item_rent_screen.dart';
 import 'package:unitory_project/providers/login_provider.dart';
 import 'package:unitory_project/user/view/login_screen.dart';
@@ -19,6 +20,14 @@ class RootTab extends StatefulWidget {
 class _RootTabState extends State<RootTab> with TickerProviderStateMixin {
   int index = 0;
   late final TabController tabController;
+  late final RefreshController refreshController;
+
+  // 페이지네이션 구현
+  final List<ItemCard> itemCards = []; // 아이템 리스트
+  DocumentSnapshot? lastDocument; // DB에서 불러온 마지막 문서
+  final int limit = 2; // 한 번에 가져올 문서 개수
+  bool hasMoreData = true; // 추가 데이터가 있는지 여부
+  bool isLoading = true; // 사용자가 스크롤 할 때만 추가적인 데이터 로드
 
   @override
   void initState() {
@@ -28,6 +37,8 @@ class _RootTabState extends State<RootTab> with TickerProviderStateMixin {
       vsync: this,
       animationDuration: Duration.zero,
     );
+
+    refreshController = RefreshController(initialRefresh: false);
   }
 
   @override
@@ -107,33 +118,50 @@ class _RootTabState extends State<RootTab> with TickerProviderStateMixin {
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       body: TabBarView(
         children: [
-          RefreshIndicator(
-            onRefresh: () async {
-              await fetchAllItems(); // 새로고침 시 fetchAllItems 호출
-              setState(() {}); // 상태를 갱신하여 UI 업데이트
-            },
-            child: FutureBuilder<List<ItemCard>>(
-              future: fetchAllItems(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return Center(child: Text('No items available.'));
-                } else {
-                  return ListView.separated(
+          FutureBuilder<List<ItemCard>>(
+            future: fetchAllItems(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                return Center(child: Text('No items available.'));
+              } else {
+                return SmartRefresher(
+                  controller: refreshController,
+                  enablePullUp: true,
+                  onRefresh: () async {
+                    // 초기화 작업
+                    lastDocument = null;
+                    itemCards.clear();
+                    hasMoreData = true;
+                    isLoading = true;
+                    await fetchAllItems(); // 새로고침 시 fetchAllItems 호출
+                    isLoading = false;
+                    setState(() {}); // 상태를 갱신하여 UI 업데이트
+
+                    refreshController.refreshCompleted();
+                  },
+                  onLoading: () async {
+                    isLoading = true;
+                    await fetchAllItems(); // 새로고침 시 fetchAllItems 호출
+                    isLoading = false;
+                    setState(() {}); // 상태를 갱신하여 UI 업데이트
+
+                    refreshController.loadComplete();
+                  },
+                  child: ListView.separated(
                     padding: EdgeInsets.all(12.0),
                     itemCount: snapshot.data!.length,
-                    itemBuilder: (context, index) {
-                      return snapshot.data![index];
+                    itemBuilder: (context, i) {
+                      return snapshot.data![i];
                     },
-                    separatorBuilder: (context, index) =>
-                        SizedBox(height: 16.0),
-                  );
-                }
-              },
-            ),
+                    separatorBuilder: (context, i) => SizedBox(height: 16.0),
+                  ),
+                );
+              }
+            },
           ),
           Container(
             color: Colors.redAccent,
@@ -189,41 +217,98 @@ class _RootTabState extends State<RootTab> with TickerProviderStateMixin {
   }
 
   Future<List<ItemCard>> fetchAllItems() async {
-    List<ItemCard> itemCards = [];
+    if (!hasMoreData) { // 더 불러올 데이터가 없는 경우 기존의 리스트 반환
+      return itemCards;
+    }
 
-    final db = FirebaseFirestore.instance;
+    if(isLoading) { // 사용자가 스크롤 할 때만 추가적인 데이터 로드
+      final db = FirebaseFirestore.instance;
 
-    // users 컬렉션의 모든 유저 문서 가져오기
-    final usersSnapshot = await db.collection("users").get();
-
-    final itemsSnapshot = await db
-        .collection("items")
-        .orderBy(
+      if (lastDocument == null) { // 맨 처음 DB에서 데이터를 불러올 때
+        // 맨 처음 DB에서 데이터를 불러올 때
+        final first = db
+            .collection("items")
+            .orderBy(
           "uploadTime",
           descending: true,
         )
-        .get();
+            .limit(limit);
 
-    for (var itemDoc in itemsSnapshot.docs) {
-      final data = itemDoc.data();
+        await first.get().then(
+              (documentSnapshots) {
+            if (documentSnapshots.docs.isNotEmpty) {
+              lastDocument = documentSnapshots.docs[documentSnapshots.size - 1];
 
-      String thumbUrl = data['thumbUrl'];
-      String title = data['title'];
-      int price = int.parse(data['price']);
-      ItemRentalPeriodType itemRentalPeriodType =
-          parseType(data['itemRentalPeriodType']);
-      DateTime uploadTime = DateTime.parse(data['uploadTime']);
+              for (var itemDoc in documentSnapshots.docs) {
+                final data = itemDoc.data();
 
-      itemCards.add(ItemCard(
-        thumbUrl: thumbUrl,
-        title: title,
-        price: price,
-        itemRentalPeriodType: itemRentalPeriodType,
-        uploadTime: uploadTime,
-      ));
+                String thumbUrl = data['thumbUrl'];
+                String title = data['title'];
+                int price = int.parse(data['price']);
+                ItemRentalPeriodType itemRentalPeriodType =
+                parseType(data['itemRentalPeriodType']);
+                DateTime uploadTime = DateTime.parse(data['uploadTime']);
+
+                itemCards.add(ItemCard(
+                  thumbUrl: thumbUrl,
+                  title: title,
+                  price: price,
+                  itemRentalPeriodType: itemRentalPeriodType,
+                  uploadTime: uploadTime,
+                ));
+              }
+            } else {
+              hasMoreData = false;
+            }
+          },
+          onError: (e) => print("Error completing: $e"),
+        );
+        isLoading = false; // 맨 처음 데이터 불러온 후에는 사용자가 스크롤 할 때만 추가적 데이터 로드
+      } else {
+        final next = db
+            .collection("items")
+            .orderBy(
+          "uploadTime",
+          descending: true,
+        )
+            .startAfterDocument(lastDocument!)
+            .limit(limit);
+
+        await next.get().then(
+              (documentSnapshots) {
+            if (documentSnapshots.docs.isNotEmpty) {
+              lastDocument = documentSnapshots.docs[documentSnapshots.size - 1];
+
+              for (var itemDoc in documentSnapshots.docs) {
+                final data = itemDoc.data();
+
+                String thumbUrl = data['thumbUrl'];
+                String title = data['title'];
+                int price = int.parse(data['price']);
+                ItemRentalPeriodType itemRentalPeriodType =
+                parseType(data['itemRentalPeriodType']);
+                DateTime uploadTime = DateTime.parse(data['uploadTime']);
+
+                itemCards.add(ItemCard(
+                  thumbUrl: thumbUrl,
+                  title: title,
+                  price: price,
+                  itemRentalPeriodType: itemRentalPeriodType,
+                  uploadTime: uploadTime,
+                ));
+              }
+            } else {
+              hasMoreData = false;
+            }
+          },
+          onError: (e) => print("Error completing: $e"),
+        );
+      }
+
+      return itemCards;
+    } else {
+      return itemCards;
     }
-
-    return itemCards;
   }
 
   ItemRentalPeriodType parseType(String type) {
